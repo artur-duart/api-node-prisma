284fc655779526ba4dd785a7f5afbd39de02435a
Artur Duarte <arturduartemoraes@gmail.com>
2024-04-15 10:49:36 -0300

    feat: update `CreateUserUseCase` with user existence check and creation

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,22 @@
+import { User } from '@prisma/client';
+import { prisma } from '../../../../prisma/client';
 import { CreateUserDTO } from './../../dtos/CreateUserDTO';
 export class CreateUserUseCase {
-	async execute({ name, email }: CreateUserDTO) {
-		return;
+	async execute({ name, email }: CreateUserDTO): Promise<User> {
+		// Verificar se o usuário já existe
+		const userAlreadyExists = await prisma.user.findUnique({
+			where: { email }
+		})
+
+		if (userAlreadyExists) {
+			// erro
+		}
+
+		// Criar o usuário
+		const user = await prisma.user.create({
+			data: { name, email },
+		})
+
+		return user;
 	}
 }
