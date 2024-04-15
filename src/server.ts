2457c57cf64c6778b806d23c3d5948d34d9f6b80
Artur Duarte <arturduartemoraes@gmail.com>
2024-04-14 21:37:42 -0300

    style: format `server.ts` file

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is running on port 3333! 🚀"));
+app.listen(3333, () => console.log('Server is running on port 3333! 🚀'));
