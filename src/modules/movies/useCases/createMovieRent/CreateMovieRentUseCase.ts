4ad0feaad0044faef1b14fb31d5853d80bffd77f
Artur Duarte <arturduartemoraes@gmail.com>
2024-04-16 15:47:32 -0300

    feat: add `CreateMovieRentUseCase.ts`

---
+++ src/modules/movies/useCases/createMovieRent/CreateMovieRentUseCase.ts
@@ -0,0 +1,38 @@
+import { AppError } from '../../../../errors/AppError';
+import { prisma } from '../../../../prisma/client';
+import { CreateMovieRentDTO } from '../../dtos/CreateMovieRentDTO';
+
+export class CreateMovieRentUseCase {
+	async execute({ movieId, userId }: CreateMovieRentDTO): Promise<void> {
+		const movieExists = await prisma.movie.findUnique({
+			where: { id: movieId },
+		});
+
+		if (!movieExists) {
+			throw new AppError('Movie not found!');
+		}
+
+		const movieAlreadyRented = await prisma.movieRent.findFirst({
+			where: { id_movie: movieId },
+		});
+
+		if (movieAlreadyRented) {
+			throw new AppError('Movie already rented!');
+		}
+
+		const userExists = await prisma.user.findUnique({
+			where: { id: userId },
+		});
+
+		if (!userExists) {
+			throw new AppError('User does not exist!');
+		}
+
+		await prisma.movieRent.create({
+			data: {
+				id_movie: movieId,
+				id_user: userId,
+			},
+		});
+	}
+}
