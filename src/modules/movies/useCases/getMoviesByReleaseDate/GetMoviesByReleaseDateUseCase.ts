cba96d013df6df03c1d7ecab42b695e9d99c8e6d
Artur Duarte <arturduartemoraes@gmail.com>
2024-04-21 18:14:12 -0300

    feat: add `GetMoviesByReleaseDateController` with handle method and `GetMoviesByReleaseDateUseCase`

---
+++ src/modules/movies/useCases/getMoviesByReleaseDate/GetMoviesByReleaseDateUseCase.ts
@@ -0,0 +1,26 @@
+import { Movie } from '@prisma/client';
+import { prisma } from '../../../../prisma/client';
+
+export class GetMoviesByReleaseDateUseCase {
+	async execute(): Promise<Movie[]> {
+    const movies = await prisma.movie.findMany({
+      orderBy: {
+        release_date: "desc"
+      },
+      include: {
+        movie_rents: {
+          select: {
+            user: {
+              select: {
+                name: true,
+                email: true
+              }
+            }
+          }
+        }
+      }
+    });
+
+		return movies;
+	}
+}
